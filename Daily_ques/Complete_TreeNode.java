a774a42e147baa76c4bb378647e0b073134bbefa
Harsh Mehrotra <harshmehrotra01@gmail.com>
2022-11-15 13:08:03 +0530

    Create Complete_TreeNode.java

---
+++ Daily_ques/Complete_TreeNode.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if(root ==null){
+            return 0;
+        }
+        int lh=leftHeight(root);
+        int rh = rightHeight(root);
+        
+        if (lh != rh){
+                    // System.out.println("hi");
+            return 1 + countNodes(root.left)+
+                countNodes(root.right);
+        }    
+        return (1 << lh) - 1;
+    }
+    
+    
+    static int leftHeight(TreeNode node){
+        int h = 0;
+        while(node!=null){
+            h++;
+            node=node.left;
+        }
+        // System.out.println(h+"l");
+        return h;
+    }
+    
+    
+    static int rightHeight(TreeNode node){
+        int h = 0;
+        while(node!=null){
+            h++;
+            node=node.right;
+        }
+                // System.out.println(h+"r");
+
+        return h;
+    }
+}
